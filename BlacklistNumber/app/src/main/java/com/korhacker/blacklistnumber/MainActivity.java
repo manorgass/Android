f1d0e37b4d8f3ddd62367740e9b104d3273f2f98
korhacker <manorgass@gmail.com>
2017-10-20 17:46:01 +0900

    DatePiacker Dialog 추가

--- BlacklistNumber/app/src/main/java/com/korhacker/blacklistnumber/MainActivity.java
+++ BlacklistNumber/app/src/main/java/com/korhacker/blacklistnumber/MainActivity.java
@@ -1,5 +1,7 @@
 package com.korhacker.blacklistnumber;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.provider.ContactsContract;
@@ -8,24 +10,47 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "DEBUG";
 
     NumberPicker npYear, npMonth, npDay;
 
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dateView = (TextView) findViewById(R.id.tv_sbd);
+
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month + 1, day);
+
         ((Button) findViewById(R.id.btn_getContact)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //getCon, tactList();
-                Toast.makeText(MainActivity.this, "유통기한: " + npYear.getValue() + "/" + npMonth.getValue() + "/" + npDay.getValue(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "유통기한: " + npYear.getValue() + "/" + npMonth.getValue() + "/" + npDay.getValue(), Toast.LENGTH_SHORT).show();
+                showDialog(999);
+                Toast.makeText(getApplicationContext(), "ca",
+                        Toast.LENGTH_SHORT)
+                        .show();
             }
         });
 
@@ -42,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         npDay.setMinValue(1);
     }
 
-    private void getContactList() {
+    /*private void getContactList() {
         ContentResolver cr = getContentResolver();
         Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
                 null, null, null, null);
@@ -72,5 +97,33 @@ public class MainActivity extends AppCompatActivity {
         if (cur != null) {
             cur.close();
         }
+    }
+*/
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    showDate(arg1, arg2 + 1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
     }
 }
